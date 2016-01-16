0075185c8406c06c1a14b30dfef11d720ca86a2f
kian <davoudi.rk@gmail.com>
2016-01-16 15:35:13 -0800

    Adding Dll Tester

---
+++ MD/DllEEB/mylib/mylib.h
@@ -0,0 +1,21 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+/*
+#if defined(BUILDING_MYLIB)
+#define MYLIB_API __declspec(dllexport) __stdcall
+#else
+#define MYLIB_API __declspec(dllimport) __stdcall
+#endif
+*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+ __declspec(dllexport)  void Hello(char *buffer);
+ __declspec(dllexport)  char  * CryptoEncrypt(char *_buffer, int _LenOfbuffer,char *_url,int _lenOfurl);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
