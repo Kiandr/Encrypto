60f0b22eb8a8739dc0b0667647e56d96d90cd663
kian <davoudi.rk@gmail.com>
2016-01-16 15:23:33 -0800

    tester

---
+++ MD/VS_Tester_DllEEB/makeMain.bat
@@ -0,0 +1,23 @@
+devenv "C:\GitHub\Encrypto\MD\DllEEB\mylib\mylib.sln" /rebuild Debug /project "C:\GitHub\Encrypto\MD\DllEEB\mylib\mylib.vcproj" /projectconfig Debug
+
+del main.exe
+del mylib.*
+ 
+
+XCOPY C:\GitHub\Encrypto\MD\DllEEB\mylib\Debug\mylib.dll C:\GitHub\Encrypto\MD\VS_Tester /s /y
+XCOPY C:\GitHub\Encrypto\MD\DllEEB\mylib\Debug\mylib.obj C:\GitHub\Encrypto\MD\VS_Tester /s /y
+XCOPY C:\GitHub\Encrypto\MD\DllEEB\mylib\Debug\mylib.lib C:\GitHub\Encrypto\MD\VS_Tester /s /y
+XCOPY C:\GitHub\Encrypto\MD\DllEEB\mylib\mylib.h C:\GitHub\Encrypto\MD\VS_Tester /s /y
+
+cd  C:\GitHub\Encrypto\MD\VS_Tester_DllEEB
+cl.exe  main.c /link mylib.lib 
+echo "Main"
+main.exe
+type example.txt
+
+
+
+ 
+
+
+
