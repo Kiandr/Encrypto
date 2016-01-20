86f1f9d28bde8eaae23228006d987e031d894ad3
kian <davoudi.rk@gmail.com>
2016-01-20 15:36:30 -0800

    Confirmation Success with Much Fewer Files

--- MD/Jan20BuildDllWrapperFourFiles/CryptoPPTestMyLib-dll/mylib/mylib.cpp
+++ MD/Jan20BuildDllWrapperFourFiles/CryptoPPTestMyLib-dll/mylib/mylib.cpp
@@ -1,7 +1,27 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "WINDOWS.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
 // ECB.cpp : Defines the exported functions for the DLL application.
 //
 //# _USE_CRT_SECURE_NO_WARNINGS
-#include "stdafx.h"
+//#include "stdafx.h"
 
 #include "mylib.h"
 #include <fstream>
